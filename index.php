c54c95caba86d505ab35695b3d243a28e72f8502
AngelineLoh <127181001+AngelineLoh@users.noreply.github.com>
2025-09-16 22:48:48 +0800

    Add Favicon

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Unicare</title>
-    <link rel="icon" href="image/logo.png" type="image/x-icon" />
+    <link rel="icon" href="../image/favicon.png" type="image/x-icon" />
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
@@ -23,7 +23,7 @@
         }, 2000);
 
         setTimeout(() => {
-            // window.location.href = 'dashboard.html';
+            window.location.href = 'Student/test.php';
         }, 3000);
     </script>
 </body>
