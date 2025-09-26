73e1720339b3ff538824fa317b3b8969a5c9bd42
BoYang125 <169112159+BoYang125@users.noreply.github.com>
2025-09-26 10:53:17 +0800

    Added create database command
    latest database file added create database command.

--- unicare(Added feeling_admin).sql
+++ unicare(Added feeling_admin).sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `unicare`
 --
+CREATE DATABASE IF NOT EXISTS `unicare` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `unicare`;
 
 -- --------------------------------------------------------
 
